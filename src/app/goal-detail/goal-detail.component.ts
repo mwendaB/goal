b5631e7f52deedcb054940f5501e2a6dc6eb21cb
mwendaB <brian.mwenda@moringaschool.com>
2021-10-06 13:23:50 +0300

    add strikethrough

--- src/app/goal-detail/goal-detail.component.ts
+++ src/app/goal-detail/goal-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-goal-detail',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GoalDetailComponent implements OnInit {
 
+  @Input() goal: Goal;
+  @Output() isComplete = new EventEmitter<boolean>();
+
+  goalComplete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
