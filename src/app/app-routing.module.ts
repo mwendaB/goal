9df56303592820d0971cadf4f26b0ec224f141fa
mwendaB <brian.mwenda@moringaschool.com>
2021-10-06 13:10:55 +0300

    add components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers:[],
+  bootstrap: [AppComponent]
 })
 export class AppRoutingModule { }
