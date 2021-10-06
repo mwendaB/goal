f99a7135a422a8187fcc0ae072b14c9b3296ad5b
mwendaB <brian.mwenda@moringaschool.com>
2021-10-06 13:52:50 +0300

    add goal-form

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -17,6 +17,16 @@ export class GoalComponent implements OnInit {
     new Goal(6,'Plot my world domination plan','Cause I am an evil overlord',new Date(2030,3,14)),
   ];
   
+  deleteGoal(isComplete, index){
+    if (isComplete) {
+      let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}?`)
+
+      if (toDelete){
+        this.goals.splice(index,1)
+      }
+    }
+  }
+  
   toggleDetails(index){
     this.goals[index].showDescription = !this.goals[index].showDescription;
   }
