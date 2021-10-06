a36e255084a03f827866bbeb792b3e8e2f9a656e
mwendaB <brian.mwenda@moringaschool.com>
2021-10-06 14:32:04 +0300

    finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +9,7 @@ import { GoalDetailComponent } from './goal-detail/goal-detail.component';
 import { StrikethroughDirective } from './strikethrough.directive';
 import { DateCountPipe } from './date-count.pipe';
 import { GoalFormComponent } from './goal-form/goal-form.component';
+
 
 @NgModule({
   declarations: [
@@ -20,7 +22,7 @@ import { GoalFormComponent } from './goal-form/goal-form.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/goal-form/goal-form.component.ts
+++ src/app/goal-form/goal-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Goal } from '../goal';
 
 @Component({
   selector: 'app-goal-form',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GoalFormComponent implements OnInit {
 
-  constructor() { }
+ 
+  newGoal = new Goal(0,"","",newDate());
+@Output() addGoal = new EventEmitter<Goal>();
 
-  ngOnInit(): void {
+  submitGoal(){
+this.addGoal.emit(this.newGoal);
+  }
+
+  ngOnInit() {
   }
 
 }
